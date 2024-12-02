d87b90b33bc4295a5d85f9f2e94269da410ba7d6
AadhithyaVenkatesan <aadhithyavenkatesan1@gmail.com>
2024-12-02 10:06:38 +0530

    Keyboard Stack

--- lib/components/usdToany.dart
+++ lib/components/usdToany.dart
@@ -12,13 +12,19 @@ class UsdToAny extends StatefulWidget {
 }
 
 class _UsdToAnyState extends State<UsdToAny> {
+  TextEditingController usdController = TextEditingController();
+  String dropdownValue = 'INR';
+  String result = 'Converted Currency';
+
   @override
   Widget build(BuildContext context) {
     return Card(
+
       color: Colors.transparent,
       child: Container(
         padding: const EdgeInsets.all(20),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
               alignment: Alignment.center,
@@ -26,6 +32,55 @@ class _UsdToAnyState extends State<UsdToAny> {
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold),),
+            ),
+            SizedBox(
+              height: 20,
+
+            ),
+            TextFormField(
+              key: Key('usd'),
+              controller: usdController,
+              cursorColor: Colors.white,
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                hintText: 'Enter USD',
+                hintStyle: TextStyle(
+                  color: Colors.white,
+                )
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: DropdownButton(
+                    items: widget.currencies.keys.toSet().toList().map<DropdownMenuItem<String>>((value){
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    dropdownColor: Colors.grey.shade900,
+                    value: dropdownValue,
+                    style: TextStyle(color: Colors.white),
+                    icon: Icon(Icons.arrow_downward,
+                    color: Colors.white,),
+                    iconSize: 24,
+                    elevation: 16,
+                    isExpanded: true,
+                    underline: Container(
+                      height: 2,
+                      color: Colors.grey.shade400,
+                    ),
+                  onChanged: (String? newValue){
+                    setState(() {
+                      dropdownValue = newValue!;
+                    });
+                  }))
+              ],
             )
           ],
         )
